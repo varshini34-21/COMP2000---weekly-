91c3faee48aa7553a83675b6e2323685f05352c7
Damian Jurd <dnj@drinkinglizard.com>
2025-08-08 16:44:52 +1000

    prep for week03.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
-public class Main {
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Main extends JFrame {
     public static void main(String[] args) throws Exception {
-        System.out.println("Red vs. Blue");
+      Main window = new Main();
+      window.run();
+    }
+
+    class Canvas extends JPanel {
+      public Canvas() {
+        setPreferredSize(new Dimension(720, 720));
+      }
+
+      @Override
+      public void paint(Graphics g) {
+	g.setColor(java.awt.Color.BLACK);
+	g.drawRect(10, 10, 700, 700);
+      }
+    }
+
+    private Main() {
+      this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      Canvas canvas = new Canvas();
+      this.setContentPane(canvas);
+      this.pack();
+      this.setVisible(true);
+    }
+
+    public void run() {
+      while(true) {
+        repaint();
+      }
     }
 }
