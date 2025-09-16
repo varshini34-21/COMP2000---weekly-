8492ea213eb11c2bac9174cfab6cadc892bebf30
varshini34-21 <varshini.vishnumohan@students.mq.edu.au>
2025-09-16 22:22:42 +1000

    Week 5: update Actor to use polygons instead of Color

--- src/Actor.java
+++ src/Actor.java
@@ -1,19 +1,20 @@
-import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Polygon;
 import java.util.List;
 
 public abstract class Actor {
-  Color color;
-  Cell loc;
-  List<Polygon> display;
+    protected Cell location;
+    protected List<Polygon> shapes;
 
-  public void paint(Graphics g) {
-    for(Polygon p: display) {
-      g.setColor(color);
-      g.fillPolygon(p);
-      g.setColor(Color.GRAY);
-      g.drawPolygon(p);
+    public Actor(Cell location, List<Polygon> shapes) {
+        this.location = location;
+        this.shapes = shapes;
     }
-  }
+
+    public void paint(Graphics g) {
+        for (Polygon p : shapes) {
+            g.drawPolygon(p);
+            g.fillPolygon(p);
+        }
+    }
 }
