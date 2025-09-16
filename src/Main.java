8d9a968846edc685b342f717966f7e0886712aab
varshini34-21 <varshini.vishnumohan@students.mq.edu.au>
2025-09-16 17:06:40 +1000

    Update Main.java to print COMP2000 2025

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Red vs. Blue");
-    }
+  public static void main(String[] args) throws Exception {
+      System.out.println("COMP2000 2025");
+  }
 }
