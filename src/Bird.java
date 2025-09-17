403edabd13c29976849b00b800be94b6320226f2
varshini34-21 <varshini.vishnumohan@students.mq.edu.au>
2025-09-17 12:49:20 +1000

    Week 7: fix Bird.java to use loc.getX()/getY() and proper display list

--- src/Bird.java
+++ src/Bird.java
@@ -1,31 +1,40 @@
 import java.awt.Color;
 import java.awt.Polygon;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Bird extends Actor {
-  public static final int birbMoves = 3;
+    public static final int birbMoves = 3;
 
-  public Bird(Cell inLoc, boolean isBot) {
-    super(inLoc, Color.GREEN, isBot, birbMoves);
-  }
+    public Bird(Cell inLoc, boolean isBot) {
+        super(inLoc, Color.GREEN, isBot, birbMoves);
+    }
 
-  protected void setPoly() {
-    display = new ArrayList<Polygon>();
-    Polygon wing1 = new Polygon();
-    wing1.addPoint(loc.x + 5, loc.y + 5);
-    wing1.addPoint(loc.x + 15, loc.y + 17);
-    wing1.addPoint(loc.x + 5, loc.y + 17);
-    Polygon wing2 = new Polygon();
-    wing2.addPoint(loc.x + 30, loc.y + 5);
-    wing2.addPoint(loc.x + 20, loc.y + 17);
-    wing2.addPoint(loc.x + 30, loc.y + 17);
-    Polygon body = new Polygon();
-    body.addPoint(loc.x + 15, loc.y + 10);
-    body.addPoint(loc.x + 20, loc.y + 10);
-    body.addPoint(loc.x + 20, loc.y + 25);
-    body.addPoint(loc.x + 15, loc.y + 25);
-    display.add(body);
-    display.add(wing1);
-    display.add(wing2);
-  }
+    @Override
+    protected void setPoly() {
+        display = new ArrayList<>();
+
+        int x = loc.getX();
+        int y = loc.getY();
+
+        Polygon wing1 = new Polygon();
+        wing1.addPoint(x + 5, y + 5);
+        wing1.addPoint(x + 15, y + 17);
+        wing1.addPoint(x + 5, y + 17);
+        display.add(wing1);
+
+        Polygon wing2 = new Polygon();
+        wing2.addPoint(x + 30, y + 5);
+        wing2.addPoint(x + 20, y + 17);
+        wing2.addPoint(x + 30, y + 17);
+        display.add(wing2);
+
+        Polygon body = new Polygon();
+        body.addPoint(x + 15, y + 10);
+        body.addPoint(x + 20, y + 10);
+        body.addPoint(x + 20, y + 25);
+        body.addPoint(x + 15, y + 25);
+        display.add(body);
+    }
 }
+
