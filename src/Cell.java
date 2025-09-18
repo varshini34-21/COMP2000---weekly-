153f3cbfc6b4db8e4d22aa2745f8fd6e26e5f68a
varshini34-21 <varshini.vishnumohan@students.mq.edu.au>
2025-09-19 02:23:12 +1000

    attempt of trying to fix the display : FAILED

--- src/Cell.java
+++ src/Cell.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class Cell {
     public int x, y;
-    public static final int SIZE = 40; 
+    public static final int SIZE = 40;
     private List<Collectible> items = new ArrayList<>();
 
     public Cell(int x, int y) {
@@ -14,12 +14,10 @@ public class Cell {
         this.y = y;
     }
 
-   
     public void addItem(Collectible item) {
         items.add(item);
     }
 
-   
     public List<Collectible> collectItems() {
         List<Collectible> temp = new ArrayList<>(items);
         items.clear();
@@ -27,17 +25,13 @@ public class Cell {
     }
 
    
-    public boolean hasItems() {
-        return !items.isEmpty();
-    }
-
-  
     public void paint(Graphics g, Point offset) {
         g.setColor(Color.LIGHT_GRAY);
+        g.fillRect(x + offset.x, y + offset.y, SIZE, SIZE);
+        g.setColor(Color.BLACK);
         g.drawRect(x + offset.x, y + offset.y, SIZE, SIZE);
     }
 
-   
     public boolean contains(Point p) {
         return p.x >= x && p.x < x + SIZE &&
                p.y >= y && p.y < y + SIZE;
